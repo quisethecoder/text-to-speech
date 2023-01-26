62e6d930430469db3fd228ca0cd011cf2cd3b8c6
quisethecoder <marquise_dawkins@aol.com>
2023-01-26 00:35:01 -0500

    voicelist const

---
+++ index.js
@@ -0,0 +1,16 @@
+const textarea = document.querySelector("textarea"),
+voiceList = document.querySelector("select"),
+speechBtn = document.querySelector("button");
+
+function textToSpeech(text){
+    let utternance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utternance);
+}
+
+speechBtn.addEventListener("click", e =>{
+    e.preventDefault();
+    if(textarea.value !== ""){
+        textToSpeech(textarea.value)
+    }
+})
+
